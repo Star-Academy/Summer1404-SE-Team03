12269663900cb026dd51f642eb15fccb1584eefd
Amirata Ghaffarian <amirata.gh.m.n@gmail.com>
2025-07-27 02:35:39 +0430

    Revert "Add project files."
    This reverts commit 2cd69b664d46e39df53314f3eaab77436b2ee77b.

--- Phase01-Project/Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Phase01_Project
-{
-    public class Student
-    {
-        public int StudentNumber { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Phase01-Solution
