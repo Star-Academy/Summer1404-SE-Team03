5f7179eb1b74a71ccaa513b92470acb003aba003
2077DevWave <2077devwave@gmail.com>
2025-08-01 13:20:06 -0400

    change SearchEngine.cs directory

--- Phase03/Core/SearchEngine.cs
+++ Phase03/Core/SearchEngine.cs
@@ -24,22 +24,11 @@ namespace SearchEngine
             searcher = new Searcher(index);
         }
 
+        
+
         public IEnumerable<string> Search(SearchQuery query)
         {
             return searcher.SmartSearch(query);
-        }
-    }
-
-    public class Program{
-        public static void Main(string[] args)
-        {
-            var dataDir = AppConfig.DataDirectory;            
-            var searchEngine = new SearchEngine(dataDir);
-            
-            var consoleUi = new ConsoleUi();
-            var query = consoleUi.GetQueryFromUser();
-            var results = searchEngine.Search(query);
-            consoleUi.DisplayResults(results);
         }
     }
 }
